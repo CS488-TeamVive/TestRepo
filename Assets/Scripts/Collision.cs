c157c4318ecb935c752628fdd0f56b1382062940
Joseph Mitchell <jmitch930@gmail.com>
2016-12-01 15:23:07 -0600

    Added basic collision, with color change to show collision

---
+++ Assets/Scripts/Collision.cs
@@ -0,0 +1,36 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Collision : MonoBehaviour {
+
+    private Renderer rend;
+    public Material defaultMat, highlightedMat;
+
+    void OnEnable()
+    {
+        EventHandlerRightController.OnTriggerClick += EventHandlerRightController_OnTriggerClick;
+    }
+
+    private void EventHandlerRightController_OnTriggerClick(object sender, VRTK.ControllerInteractionEventArgs e)
+    {
+        this.gameObject.SetActive(false);
+    }
+
+    // Use this for initialization
+    void Start ()
+    {
+        rend = GetComponent<Renderer>();
+    }
+
+    void OnTriggerEnter(Collider col)
+    {
+        Debug.Log("Trigger Enter");
+        rend.sharedMaterial = highlightedMat;
+    }
+
+    void OnTriggerExit(Collider col)
+    {
+        Debug.Log("Trigger Exit");
+        rend.sharedMaterial = defaultMat;
+    }
+}
